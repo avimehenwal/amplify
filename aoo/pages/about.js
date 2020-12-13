b8f18a359a819681dc71078b66d8f8d51a0a70ba
avimehenwal <avi.mehanwal@gmail.com>
2020-12-13 22:30:26 +0100

    feat(component): sample component

---
+++ aoo/pages/about.js
@@ -0,0 +1,13 @@
+import { Welcome } from '../components/Text'
+
+export default function AboutPage() {
+  return (
+    <section>
+      <Welcome name="Sara" />
+      <Welcome name="Dani" />
+      <Welcome name="Mahi" />
+      <Welcome name="Uruk" />
+    </section>
+  )
+}
+
